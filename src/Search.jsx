b30c37c22e655e70b8c56af411434076a817f731
S.M.ABTAHI NOOR <73340940+19smabtahinoor@users.noreply.github.com>
2021-05-02 15:43:06 +0600

    Version 1.2

--- src/Search.jsx
+++ src/Search.jsx
@@ -1,15 +1,26 @@
 import React,{useState} from 'react'
+import axios from 'axios'
 import SearchIcon from '@material-ui/icons/Search';
 import Button from '@material-ui/core/Button';
-import SearchImage from './SearchImage'
 import {Link} from 'react-router-dom'
 
 function Search(){
 
     const [img,setImg] = useState('')
+    const [clientId,setClientID] = useState('7Ba_eQo9MNRsx10o7i0BgDVe8bIZIuR85PShZqsfdAw')
+    const [result,setResult] = useState([])
+
 
     const searchEvent = (event) => {
-        setImg(event.target.value)
+        setImg(event.target.value) 
+    }
+
+    const handleSubmit = () => {
+        const url = `https://api.unsplash.com/search/photos?page=1&query=${img}&client_id=${clientId}`
+
+        axios.get(url).then(
+            response => setResult(response.data.results)
+        )
     }
     return(
         <>
@@ -19,8 +30,8 @@ function Search(){
             </div>
 
             <div className="searchBar">
-                <Button className="search_btn">
-                <SearchIcon/>
+                <Button className="search_btn" onClick={handleSubmit}>
+                    <SearchIcon/>
                 </Button>
                 
                 <input type="text" placeholder="Search Images" className="search_input" value={img} onChange={searchEvent} />
@@ -39,13 +50,26 @@ function Search(){
             </div>
         </header>
 
-        {img === '' ? 
+
+
+    {img === '' ? 
         <div>
         <h1 className="searchAnyText">Search any images</h1>
         <img src="https://i.ibb.co/B6s6bKR/House-searching-cuate.png" alt="src" className="searching_img" /> 
         </div>
-        : <SearchImage imageName={img}/>}
+        : <div className="image_container">
+        {result.map( (photos,index) => (
+                    <div className="search_result_area" key={index}>
+                        <a download>
+                        <img  src={photos.urls.regular} alt="randomimage"/>
+                        </a>
+
+                    </div> 
+        ))}
+        </div>
+        }
         </>
     )
 }
 export default Search
+
